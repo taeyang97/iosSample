b6b8afe9a3081231012daffd187aadaf47d7d80b
zentrykty <jerry@zentry.kr>
2023-08-22 14:28:50 +0900

    Initial Commit

---
+++ firstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstProject
+//
+//  Created by 김태양 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
